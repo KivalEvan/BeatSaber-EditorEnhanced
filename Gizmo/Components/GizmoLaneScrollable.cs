cb122172f66844c19177318f59af0274d14b4dc9
Kival Evan <5645391+KivalEvan@users.noreply.github.com>
2026-07-20 01:11:16 +0800

    Fix gizmo lane scroll not check alt

--- Gizmo/Components/GizmoLaneScrollable.cs
+++ Gizmo/Components/GizmoLaneScrollable.cs
@@ -1,6 +1,7 @@
 using BeatmapEditor3D;
 using EditorEnhanced.Commands;
 using UnityEngine;
+using UnityEngine.InputSystem;
 using Zenject;
 
 namespace EditorEnhanced.Gizmo.Components;
@@ -18,7 +19,7 @@ public class GizmoLaneScrollable : MonoBehaviour, IGizmoScrollInput, IGizmoPoola
 
    public void OnScroll(float delta)
    {
-      if (EventBoxEditorDataContext == null) return;
+      if (EventBoxEditorDataContext == null || !Keyboard.current.altKey.isPressed) return;
       _signalBus.Fire(new ScrollGizmoEventBoxLaneSignal(EventBoxEditorDataContext, delta));
    }
 }
